7457fe375b7a0734a2dc60e3305cd44694258777
navneetnavs <navneetkumar2004@icloud.com>
2024-09-15 00:16:12 +0530

    commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const path = require("path");
+const http = require("http");
+const socketio = require("socket.io");
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.set("view engine", "ejs");
+app.set(express.static(path.join(__dirname,"public")));
+
+io.on("connection", function(socket){
+    socket.on("send-location",function(data){
+        io.emit("recieve-location",{id:socket.id, ...data});
+    });
+    socket.on("disconnected", function(){
+        io.emit("user-disconnect",socket.id)
+    })
+})
+
+app.get("/", function(req,res){
+ res.render("index")
+});
+
+server.listen(4000);
